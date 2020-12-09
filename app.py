e59857d9d81629007b98376d05e28531886b90e2
nadav pesach <nadav-pesach@users.noreply.github.com>
2020-12-09 22:22:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -169,8 +169,8 @@ def answer():
             query = Users.select(Users.user_name, peewee.fn.COUNT().alias('total_points')).join(Games).join(GameResulte).group_by(Users.user_name).order_by(Users.user_name.desc()).limit(3)
             top_players = []
             try:
-            for user in query:
-                top_players += [user.user_name, user.total_points]
+                for user in query:
+                    top_players += [user.user_name, user.total_points]
             except peewee.ProgrammingError as err:
                 print(err)
             return render_template('index.j2', game_played=game_played, top_players=top_players, resulte=resulte)
